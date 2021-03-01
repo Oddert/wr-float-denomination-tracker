8cdc2a920e4ee1826541a89698b92e1583055470
Robyn Felicity Heather Veitch <oddert@gmail.com>
2021-03-01 02:41:10 +0000

    added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { Router, Switch } from 'react-router'
+import { useHistory } from 'react-router-dom'
 
-function App() {
+const App: React.FC = () => {
+  const history = useHistory()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div className="App">
+        &lt;
+        Side Menu
+        &gt;
+        <br/>
+        &lt;
+        Header
+        &gt;
+        <Switch>
+          
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
